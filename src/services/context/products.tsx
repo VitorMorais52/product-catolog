2c76c5ebb312e951f29c87199320990f4ac57f3f
Morais <vitormorais52@gmail.com>
2022-02-25 00:42:53 -0300

    add new function to productsContext

--- src/services/context/products.tsx
+++ src/services/context/products.tsx
@@ -8,16 +8,21 @@ type Product = {
   price: number;
   rating: Record<string, number>;
   title: string;
+  quantity: number;
 };
 
 type ProductsContextProps = {
   products: Product[];
   storeProducts: (data: Product[]) => void;
+  addSingleProduct: (data: Product) => void;
+  quantityItems: () => number;
 };
 
 const DEFAULT_VALUE = {
   products: [] as Product[],
   storeProducts: () => {},
+  addSingleProduct: () => {},
+  quantityItems: () => 0,
 };
 
 export const ProductsContext =
@@ -30,8 +35,39 @@ export const ProductsProvider: React.FC = ({ children }) => {
     setProducts(data);
   };
 
+  const addSingleProduct = (data: Product) => {
+    const indexAlreadyExists = products.findIndex(
+      (product) => product.id === data.id
+    );
+
+    if (indexAlreadyExists !== -1) {
+      const oldProduct = products[indexAlreadyExists];
+
+      const hasPropertyQuantity = Reflect.has(oldProduct, "quantity");
+
+      const newProducts = products;
+      if (hasPropertyQuantity) newProducts[indexAlreadyExists].quantity += 1;
+      else newProducts[indexAlreadyExists] = { ...oldProduct, quantity: 1 };
+
+      setProducts([...newProducts]);
+    } else {
+      setProducts([...products, { ...data, quantity: 1 }]);
+    }
+  };
+
+  const quantityItems = () => {
+    const quantity = products.reduce(
+      (previousValue, { quantity: currentValue }) =>
+        previousValue + currentValue,
+      0
+    );
+    return quantity;
+  };
+
   return (
-    <ProductsContext.Provider value={{ products, storeProducts }}>
+    <ProductsContext.Provider
+      value={{ products, storeProducts, addSingleProduct, quantityItems }}
+    >
       {children}
     </ProductsContext.Provider>
   );
